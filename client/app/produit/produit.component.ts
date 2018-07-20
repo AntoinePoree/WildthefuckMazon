dc561a53e828db5d48418fcc48fbd03970d6e944
AntoinePoree <poree47@gmail.com>
2018-07-20 15:54:27 +0200

    idk why this doesnt work

--- client/app/produit/produit.component.ts
+++ client/app/produit/produit.component.ts
@@ -2,6 +2,9 @@ import { AddProduitService } from './../services/addProduit.service';
 import { Component, OnInit } from '@angular/core';
 import { AddProduit } from '../shared/models/addProduit.model';
 import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { User } from '../shared/models/user.model';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-produit',
@@ -9,28 +12,43 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./produit.component.css'],
 })
 export class ProduitComponent implements OnInit {
-  addProduit = new AddProduit();
-  addProduits: AddProduit[] = [];
+  addProduit;
+  addProduits: AddProduit[];
   isLoading = true;
   isEditing = false;
+  user: User;
 
   constructor(
+    private auth: AuthService,
     private addProduitService: AddProduitService,
     private route: ActivatedRoute,
     private router: Router,
+    private userService: UserService,
   ) {}
 
   ngOnInit() {
-    this.getAddProduits();
+    this.getAddProduit();
+    this.getUser();
   }
-  getAddProduits() {
+  getUser() {
+    this.userService.getUser(this.auth.currentUser).subscribe(
+      data => this.user = data,
+      error => console.log(error),
+      () => this.isLoading = false,
+    );
+  }
+  getAddProduit() {
     this.addProduitService.getAddProduits().subscribe(
         data => (this.addProduits = data, console.log(data)),
         error => console.log(error),
         () => (this.isLoading = false),
       );
   }
-  buyitem() {
-    console.log('1');
+  buyitem(addProduit) {
+    // this.addProduits = addProduit;
+    // this.user.sold -= addProduit.price;
+    // this.admin.sold += addProduit.price;
+    console.log(this.addProduit);
+    console.log(this.user.sold);
   }
 }
